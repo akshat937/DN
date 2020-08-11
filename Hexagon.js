930203b761a46711a9b59ac514f3d64d7974c5a0
akshat937 <67277563+akshat937@users.noreply.github.com>
2020-08-11 19:07:22 +0530

    Add files via upload

---
+++ Hexagon.js
@@ -0,0 +1,18 @@
+class Hexagon{
+    constructor(x,y,radius) {
+        var options = {
+            isStatic : false,
+    restitution : 0.3,
+    friction : 0.5,
+    density : 1.2
+        }
+        this.r = radius;
+        this.body = Bodies.circle(x,y,this.r,options);
+        World.add(world, this.body);
+      }
+      display(){
+        fill("255,0,255");
+        ellipseMode(CENTER);
+        ellipse(this.body.position.x,this.body.position.y,this.r,this.r);
+      }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# DN
+# p5.play-boilerplate
+Boiler plate for p5.play
